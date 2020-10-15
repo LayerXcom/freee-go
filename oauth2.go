86bb6bc307ade931143263e3a6c8eccc92e8b3d7
yyoshiki41 <yyoshiki41@gmail.com>
2020-10-15 16:05:32 +0900

    feat(freee): implements Exchange method

--- oauth2.go
+++ oauth2.go
@@ -1,9 +1,15 @@
 package freee
 
 import (
+	"context"
+
 	"golang.org/x/oauth2"
 )
 
 func (c *Client) AuthCodeURL(state string, opts ...oauth2.AuthCodeOption) string {
 	return c.config.Oauth2.AuthCodeURL(state, opts...)
+}
+
+func (c *Client) Exchange(ctx context.Context, code string, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
+	return c.config.Oauth2.Exchange(ctx, code, opts...)
 }
