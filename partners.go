ff0a559902aff1e5313e94c0cf7eedb0e25e2735
yyoshiki41 <yyoshiki41@gmail.com>
2020-11-02 21:22:51 +0900

    fix(partners): fixed delete bugs

--- partners.go
+++ partners.go
@@ -285,14 +285,12 @@ func (c *Client) DestroyPartner(
 	ctx context.Context, oauth2Token *oauth2.Token,
 	companyID uint32, partnerID int32,
 ) (*oauth2.Token, error) {
-	var result interface{}
-
 	v, err := query.Values(nil)
 	if err != nil {
 		return oauth2Token, err
 	}
 	SetCompanyID(&v, companyID)
-	tokenSource, err := c.call(ctx, path.Join(APIPathManualJournals, fmt.Sprint(partnerID)), http.MethodDelete, oauth2Token, v, nil, &result)
+	tokenSource, err := c.call(ctx, path.Join(APIPathManualJournals, fmt.Sprint(partnerID)), http.MethodDelete, oauth2Token, v, nil, nil)
 	if err != nil {
 		return oauth2Token, err
 	}
