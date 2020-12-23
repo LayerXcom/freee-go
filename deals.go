02a4cc367c740c4f60585c1a0fcaa72d4289d930
Nao Hanamura <naomasabit@gmail.com>
2020-12-23 15:41:11 +0900

    feat: add deals.go

--- deals.go
+++ deals.go
@@ -13,28 +13,102 @@ const (
 	APIPathDeals = "deals"
 )
 
-type DealResponse struct {
-	Deal Deal `json:"deal"`
+type DealCreateResponse struct {
+	Deal DealCreateResponseDeal `json:"deal"`
 }
 
-type Deal struct {
-	// 発生日
-	IssueDate string `json:"issue_date"`
-	// 収支区分
-	Type string `json:"type"`
+// DealCreateResponseDeal struct for DealCreateResponseDeal
+type DealCreateResponseDeal struct {
+	// 取引ID
+	Id int32 `json:"id"`
 	// 事業所ID
-	CompanyID int32 `json:"company_id"`
+	CompanyId int32 `json:"company_id"`
 	// 発生日 (yyyy-mm-dd)
-	DueDate string `json:"due_date"`
+	IssueDate string `json:"issue_date"`
+	// 支払期日 (yyyy-mm-dd)
+	DueDate *string `json:"due_date,omitempty"`
+	// 金額
+	Amount int32 `json:"amount"`
+	// 支払金額
+	DueAmount *int32 `json:"due_amount,omitempty"`
+	// 収支区分 (収入: income, 支出: expense)
+	Type *string `json:"type,omitempty"`
 	// 取引先ID
-	PartnerID bool `json:"partner_id"`
+	PartnerId int32 `json:"partner_id"`
 	// 取引先コード
-	PartnerCode bool `json:"partner_code"`
+	PartnerCode string `json:"partner_code,omitempty"`
 	// 管理番号
-	RefNumber *string `json:"ref_number"`
-	// 貸借行一覧（配列）: 貸借合わせて100行まで登録できます。
-	Details []DealUpdateParams `json:"details"`
+	RefNumber *string `json:"ref_number,omitempty"`
+	// 決済状況 (未決済: unsettled, 完了: settled)
+	Status string `json:"status"`
+	// 取引の明細行
+	Details *[]DealCreateResponseDealDetails `json:"details,omitempty"`
+	// 取引の支払行
+	Payments *[]DealCreateResponseDealPayments `json:"payments,omitempty"`
 }
+
+// DealCreateResponseDealDetails struct for DealCreateResponseDealDetails
+type DealCreateResponseDealDetails struct {
+	// 取引行ID
+	Id int32 `json:"id"`
+	// 勘定科目ID
+	AccountItemId int32 `json:"account_item_id"`
+	// 税区分コード
+	TaxCode int32 `json:"tax_code"`
+	// 品目ID
+	ItemId int32 `json:"item_id,omitempty"`
+	// 部門ID
+	SectionId int32 `json:"section_id,omitempty"`
+	// メモタグID
+	TagIds *[]int32 `json:"tag_ids,omitempty"`
+	// セグメント１ID
+	Segment1TagId int32 `json:"segment_1_tag_id,omitempty"`
+	// セグメント２ID
+	Segment2TagId int32 `json:"segment_2_tag_id,omitempty"`
+	// セグメント３ID
+	Segment3TagId int32 `json:"segment_3_tag_id,omitempty"`
+	// 取引金額
+	Amount int32 `json:"amount"`
+	// 消費税額
+	Vat int32 `json:"vat"`
+	// 備考
+	Description *string `json:"description,omitempty"`
+	// 貸借（貸方: credit, 借方: debit）
+	EntrySide string `json:"entry_side"`
+}
+
+// DealCreateResponseDealPayments struct for DealCreateResponseDealPayments
+type DealCreateResponseDealPayments struct {
+	// 取引行ID
+	Id int32 `json:"id"`
+	// 支払日
+	Date string `json:"date"`
+	// 口座区分 (銀行口座: bank_account, クレジットカード: credit_card, 現金: wallet, プライベート資金（法人の場合は役員借入金もしくは役員借入金、個人の場合は事業主貸もしくは事業主借）: private_account_item)
+	FromWalletableType *string `json:"from_walletable_type,omitempty"`
+	// 口座ID（from_walletable_typeがprivate_account_itemの場合は勘定科目ID）
+	FromWalletableId *int32 `json:"from_walletable_id,omitempty"`
+	// 支払金額
+	Amount int32 `json:"amount"`
+}
+
+//type Deal struct {
+//	// 発生日
+//	IssueDate string `json:"issue_date"`
+//	// 収支区分
+//	Type string `json:"type"`
+//	// 事業所ID
+//	CompanyID int32 `json:"company_id"`
+//	// 発生日 (yyyy-mm-dd)
+//	DueDate string `json:"due_date"`
+//	// 取引先ID
+//	PartnerID bool `json:"partner_id"`
+//	// 取引先コード
+//	PartnerCode bool `json:"partner_code"`
+//	// 管理番号
+//	RefNumber *string `json:"ref_number"`
+//	// 貸借行一覧（配列）: 貸借合わせて100行まで登録できます。
+//	Details []DealUpdateParams `json:"details"`
+//}
 
 // DealCreateParams struct for DealCreateParams
 type DealCreateParams struct {
@@ -110,94 +184,13 @@ func NewDealCreateParams(issueDate string, type_ string, companyId int32, detail
 	return &this
 }
 
-// DealUpdateParams struct for DealUpdateParams
-type DealUpdateParams struct {
-	// 発生日 (yyyy-mm-dd)
-	IssueDate string `json:"issue_date"`
-	// 収支区分 (収入: income, 支出: expense)
-	Type string `json:"type"`
-	// 事業所ID
-	CompanyId int32 `json:"company_id"`
-	// 支払期日(yyyy-mm-dd)
-	DueDate *string `json:"due_date,omitempty"`
-	// 取引先ID
-	PartnerId *int32 `json:"partner_id,omitempty"`
-	// 取引先コード
-	PartnerCode *string `json:"partner_code,omitempty"`
-	// 管理番号
-	RefNumber *string                   `json:"ref_number,omitempty"`
-	Details   []DealUpdateParamsDetails `json:"details"`
-	// 証憑ファイルID（配列）
-	ReceiptIds []int32 `json:"receipt_ids,omitempty"`
-}
-
-func NewDealUpdateParams(issueDate string, type_ string, companyId int32, details []DealUpdateParamsDetails) *DealUpdateParams {
-	this := DealUpdateParams{}
-	this.IssueDate = issueDate
-	this.Type = type_
-	this.CompanyId = companyId
-	this.Details = details
-	return &this
-}
-
-// DealUpdateParamsDetails struct for DealUpdateParamsDetails
-type DealUpdateParamsDetails struct {
-	// 取引行ID: 既存取引行を更新する場合に指定します。IDを指定しない取引行は、新規行として扱われ追加されます。また、detailsに含まれない既存の取引行は削除されます。更新後も残したい行は、必ず取引行IDを指定してdetailsに含めてください。
-	Id *int32 `json:"id,omitempty"`
-	// 税区分コード
-	TaxCode int32 `json:"tax_code"`
-	// 勘定科目ID
-	AccountItemId int32 `json:"account_item_id"`
-	// 取引金額（税込で指定してください）
-	Amount int32 `json:"amount"`
-	// 品目ID
-	ItemId *int32 `json:"item_id,omitempty"`
-	// 部門ID
-	SectionId *int32 `json:"section_id,omitempty"`
-	// メモタグID
-	TagIds *[]int32 `json:"tag_ids,omitempty"`
-	// セグメント１ID
-	Segment1TagId *int32 `json:"segment_1_tag_id,omitempty"`
-	// セグメント２ID
-	Segment2TagId *int32 `json:"segment_2_tag_id,omitempty"`
-	// セグメント３ID
-	Segment3TagId *int32 `json:"segment_3_tag_id,omitempty"`
-	// 備考
-	Description *string `json:"description,omitempty"`
-	// 消費税額（指定しない場合は自動で計算されます）
-	Vat *int32 `json:"vat,omitempty"`
-}
-
-// NewDealUpdateParamsDetails instantiates a new DealUpdateParamsDetails object
-func NewDealUpdateParamsDetails(taxCode int32, accountItemId int32, amount int32) *DealUpdateParamsDetails {
-	this := DealUpdateParamsDetails{}
-	this.TaxCode = taxCode
-	this.AccountItemId = accountItemId
-	this.Amount = amount
-	return &this
-}
-
 func (c *Client) CreateDeal(
 	ctx context.Context, oauth2Token *oauth2.Token,
 	params CreateManualJournalParams,
-) (*ManualJournalResponse, *oauth2.Token, error) {
-	var result ManualJournalResponse
+) (*DealCreateResponse, *oauth2.Token, error) {
+	var result DealCreateResponse
 
-	oauth2Token, err := c.call(ctx, APIPathManualJournals, http.MethodPost, oauth2Token, nil, params, &result)
-	if err != nil {
-		return nil, oauth2Token, err
-	}
-
-	return &result, oauth2Token, nil
-}
-
-func (c *Client) UpdateDeal(
-	ctx context.Context, oauth2Token *oauth2.Token,
-	params DealUpdateParams,
-) (*DealResponse, *oauth2.Token, error) {
-	var result DealResponse
-
-	oauth2Token, err := c.call(ctx, APIPathManualJournals, http.MethodPost, oauth2Token, nil, params, &result)
+	oauth2Token, err := c.call(ctx, APIPathDeals, http.MethodPost, oauth2Token, nil, params, &result)
 	if err != nil {
 		return nil, oauth2Token, err
 	}
