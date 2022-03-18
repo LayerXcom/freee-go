b7b53e57316372f6768deaaec524afdaa5d5af6f
shirakiyo <tus.sk5678@gmail.com>
2022-03-18 13:31:54 +0900

    fix

--- journals.go
+++ journals.go
@@ -48,15 +48,15 @@ type JournalUpToDateReasons struct {
 
 type GetJournalsOpts struct {
 	// ダウンロード形式
-	DownloadType string `json:"download_type"`
+	DownloadType string `url:"download_type"`
 	// 補助科目やコメントとして出力する項目
-	VisibleTags []string `json:"visible_tags,omitempty"`
+	VisibleTags []string `url:"visible_tags,omitempty"`
 	// 追加出力するID項目
-	VisibleIDs []string `json:"visible_ids,omitempty"`
+	VisibleIDs []string `url:"visible_ids,omitempty"`
 	// 取得開始日 (yyyy-mm-dd)
-	StartDate string `json:"start_date,omitempty"`
+	StartDate string `url:"start_date,omitempty"`
 	// 取得終了日 (yyyy-mm-dd)
-	EndDate string `json:"end_date,omitempty"`
+	EndDate string `url:"end_date,omitempty"`
 }
 
 func (c *Client) GetJournals(
