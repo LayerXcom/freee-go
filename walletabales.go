26b2300f39988335c3c86ad59f8e3a1527d88257
yyoshiki41 <yyoshiki41@gmail.com>
2022-09-01 13:23:18 +0900

    fix(walletable): set wallet type

--- walletabales.go
+++ walletabales.go
@@ -3,24 +3,30 @@ package freee
 import (
 	"context"
 	"fmt"
-	"github.com/google/go-querystring/query"
-	"golang.org/x/oauth2"
 	"net/http"
 	"path"
+
+	"github.com/google/go-querystring/query"
+	"golang.org/x/oauth2"
 )
 
 const (
 	APIPathWalletables = "walletables"
+
+	// 口座種別
+	WalletTypeBankAccount = "bank_account" // 銀行口座
+	WalletTypeCreditCard  = "credit_card"  // クレジットカード
+	WalletTypeWallet      = "wallet"       //現金, その他の決済口座
 )
 
 type WalletablesResponse struct {
 	Walletables []Walletable `json:"walletables"`
-	Meta		Meta         `json:"meta"`
+	Meta        Meta         `json:"meta"`
 }
 
 type WalletableResponse struct {
 	Walletable Walletable `json:"walletable"`
-	Meta		Meta         `json:"meta"`
+	Meta       Meta       `json:"meta"`
 }
 
 type Meta struct {
@@ -61,7 +67,7 @@ func (c *Client) GetWalletables(
 	}
 
 	SetCompanyID(&v, companyID)
-	oauth2Token, err = c.call(ctx, path.Join(APIPathWalletables), http.MethodGet,oauth2Token, v, nil, &result)
+	oauth2Token, err = c.call(ctx, path.Join(APIPathWalletables), http.MethodGet, oauth2Token, v, nil, &result)
 	if err != nil {
 		return nil, oauth2Token, err
 	}
@@ -69,20 +75,19 @@ func (c *Client) GetWalletables(
 	return &result, oauth2Token, nil
 }
 
-
 func (c *Client) GetWalletable(
 	ctx context.Context, oauth2Token *oauth2.Token,
-	companyID uint32, walletableID uint64, opts GetWalletTxnOpts,
-) (*Walletable,  *oauth2.Token, error) {
+	companyID uint32, walletType string, walletableID uint64,
+) (*Walletable, *oauth2.Token, error) {
 	var result WalletableResponse
 
-	v, err := query.Values(opts)
+	v, err := query.Values(nil)
 	if err != nil {
 		return nil, oauth2Token, err
 	}
 
 	SetCompanyID(&v, companyID)
-	oauth2Token, err = c.call(ctx, path.Join(APIPathWalletables, fmt.Sprint(walletableID)), http.MethodGet, oauth2Token, v, nil, &result)
+	oauth2Token, err = c.call(ctx, path.Join(APIPathWalletables, walletType, fmt.Sprint(walletableID)), http.MethodGet, oauth2Token, v, nil, &result)
 	if err != nil {
 		return nil, oauth2Token, err
 	}
